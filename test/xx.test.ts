284def61394acc44a667ff3450aa269e3e979c32
Kalin Azgorov <kalin.azgorov@gmail.com>
2024-01-15 12:45:37 +0200

    Initial commit

---
+++ test/xx.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Xx from '../lib/xx-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/xx-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Xx.XxStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
